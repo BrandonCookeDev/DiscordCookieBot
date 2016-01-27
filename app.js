0a5715bc3b1a07499016391e4c5725689bd7b8b5
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-01-27 14:46:48 -0500

    Added logic to send file streams instead of just URLs

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+/** IMPORTS **/
 var Discord = require("discord.js");
-var mybot = new Discord.Client();
+var fs 		= require("fs");
+var mybot 	= new Discord.Client();
 
-var bracketUrl = 'http://challonge.com/fbgt21';
+/** CONFIG **/
+var servers = [];
+var channelMap = {};
+
+/** NONSENSE **/
+/*** IMAGES ***/
+var bruceImg  = "./content/images/Brucie.PNG";
 var falconImg = "./content/images/CFalc.png";
+
+/*** URLs ***/
+var bracketUrl = 'http://challonge.com/fbgt21';
 var falconUrl = 'https://upload.wikimedia.org/wikipedia/en/4/4c/Captain_Falcon_character_portrait.png';
 var bruceUrl = 'http://imgur.com/90kq6VT';
 var suhdudeUrl = 'https://www.youtube.com/watch?v=pIHYPaoh79I';
+
+/*** MESSAGES ***/
+var bracket = "Flashback Games 21: " + bracketUrl;
 
 console.log("Running cookiE_bot...");
 
@@ -16,19 +30,81 @@ mybot.on("message", function(message){
     if(message.content.charAt(0) === "!")
     {
     	var command = message.content.substring(1).toLowerCase();
+    	/** SERIOUS **/
+    	if(command.substring(1, 4) === "mute")
+    	{
+    		var memberName = command.substring(command.indexOf(" "));
+    		try{
+	    		var server = message.channel.server;
+	    		var user = server.members.get("username", memberName);
+	    		
+	    	}
+	    	catch(err)
+	    	{
+	    		mybot.reply(message, "The user " + memberName + " isn't in the chat");
+	    	}     		
+    	}
+    	
+    	/** SILLY **/
     	if(command === "bracket")
-    		mybot.reply(message, bracketUrl).catch(console.log);
+    		mybot.reply(message, bracket).catch(console.log);
     	
     	if(command === "showmeyourmoves")
-    		mybot.reply(message, falconUrl).catch(console.log);
+    	{
+    		try{
+    			var falconStream = fs.createReadStream(falconImg);
+    			mybot.sendFile(message.channel, falconStream, "CFalc.png");
+    		}
+    		catch(err){
+    			console.log(err);
+    			mybot.reply(message, falconUrl).catch(console.log);
+    		}
+    	}
     		
     	if(command === "bruciepie")
-    		mybot.reply(message, bruceUrl);
+    	{
+    		try{
+	    		var bruceStream = fs.createReadStream(bruceImg);
+	    		mybot.sendFile(message.channel, bruceStream, "Brucie.png")
+	    	}
+	    	catch(err)
+	    	{
+	    		console.log(err);
+	    		mybot.reply(message, bruceUrl);	
+	    	}
+    		
+    	}
     		
     	if(command === "suhdude")
     		mybot.reply(message, suhdudeUrl);
     }
 });
 
-mybot.login("golee5191@hotmail.com", "botmedaddy!").catch(console.log); 	  //TEST 
-//mybot.login("ckscookiessbm@gmail.com", "botmedaddy!").catch(console.log);   //PROD
+/** LOGIN **/
+mybot.login("golee5191@hotmail.com", "botmedaddy!").then(loginSuccess).catch(console.log); 	  //TEST 
+//mybot.login("ckscookiessbm@gmail.com", "botmedaddy!").then(loginSuccess).catch(console.log);   //PROD
+
+/** FUNCTIONS **/
+function loginSuccess(token)
+{	
+	if(mybot.channels.has("name", "test"))
+	{
+		//TELL ALL CHANNELS BOT IS ONLINE
+		mybot.channels.getAll("name", "test").foreach(function(){
+			mybot.sendMessage(channel, "...cookie_bot is Online...");
+		})
+		.then("Notified clients...")
+		.catch(console.log);
+	}
+		
+	//})
+	//.then("Notified servers...")
+	//.catch(console.log);
+}
+
+function getChannels()
+{
+	var server = new ServerChannel();
+	
+}
+
