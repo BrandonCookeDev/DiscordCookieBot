d660fed408fec011bb4a157a30cd7d56a23f9eb5
Brandon Cooke <BrandonCookeDev@gmail.com>
2016-02-15 00:52:33 -0500

    Setting up Twitter Auth

--- twitter.js
+++ twitter.js
@@ -1,1 +1,16 @@
 var twitter = require('twitter');
+var props	= require('./twitter.properties');
+
+var client = new Twitter({
+	  consumer_key: props.consumer_key,
+	  consumer_secret: props.consumer_secret,
+	  access_token_key: props.access_token_key,
+	  access_token_secret: props.access_token_secret
+	});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
