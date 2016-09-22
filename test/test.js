041e650cdc36b4c70658762702c40a49dc620aea
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-09-22 13:07:42 -0400

    Added folder for holding unit tests, also dependency for Mocha unit testing framework.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert  = require('assert');
+
+var seriousCmd   = require('../commands/seriousCommands');
+var dumbCmd      = require('../commands/dumbCommands');
+var copypastaCmd = require('../commands/copypastaCommands');
+var imageCmd     = require('../commands/imageCommands');
+var modeCmd      = require('../commands/modeCommands');
+var videoCmd     = require('../commands/videoCommands');
+
+/** TEST SERIOUS COMMANDS **/
